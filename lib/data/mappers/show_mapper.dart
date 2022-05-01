f9b5b55610d95909453350d4b0056604118f204d
KokorinAlex <a.kokorin73@mail.ru>
2022-05-01 18:23:28 +0400

    extension

--- lib/data/mappers/show_mapper.dart
+++ lib/data/mappers/show_mapper.dart
@@ -1,8 +1,15 @@
 import 'package:project_video/app/models/film_card_model.dart';
 import 'package:project_video/data/dtos/show_card_dto.dart';
 
-// extension ShowCardDtoToDomain on ShowCardDTO{
-//   FilmCardModel toDomain(){
-//     return FilmCardModel(id: id, title: title, )
-//   }
-// }
+extension ShowCardDtoToDomain on ShowCardDTO {
+  FilmCardModel toDomain() {
+    return FilmCardModel(
+      id: show?.id ?? 0,
+      title: show?.title ?? '',
+      picture: show?.picture?.original ?? '',
+      releaseDate: show?.releaseDate,
+      voteAverage: show?.voteAverage?.average,
+      description: show?.description,
+    );
+  }
+}
