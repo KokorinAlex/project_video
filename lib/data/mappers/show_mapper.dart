bb5f52136d22f4876e26e828ac8444975a3b9da4
KokorinAlex <a.kokorin73@mail.ru>
2022-05-01 15:51:48 +0400

    correct DTO

---
+++ lib/data/mappers/show_mapper.dart
@@ -0,0 +1,8 @@
+import 'package:project_video/app/models/film_card_model.dart';
+import 'package:project_video/data/dtos/show_card_dto.dart';
+
+// extension ShowCardDtoToDomain on ShowCardDTO{
+//   FilmCardModel toDomain(){
+//     return FilmCardModel(id: id, title: title, )
+//   }
+// }
