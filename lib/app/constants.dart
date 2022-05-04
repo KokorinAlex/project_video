796bb772f93d61c89d5b12bb139decb2b8735cc6
KokorinAlex <a.kokorin73@mail.ru>
2022-05-04 22:21:00 +0400

    add repository

---
+++ lib/app/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class MovieQuery {
+  static const String baseUrl = 'https://api.tvmaze.com/search/shows';
+  static const String pisecImageUrl =
+      'http://risovach.ru/upload/2014/08/mem/sashko_57920362_orig_.jpg';
+  static const String nothingImageUrl =
+      'http://risovach.ru/upload/2016/03/mem/klichko_108546640_orig_.jpg';
+
+  static const String initialQ = 'friends';
+}
+
+class MovieLocal {
+  static const String error = 'Ошибка';
+  static const String unknown = 'Неизвестно';
+  static const String ratingPrefix = 'Оценка: ';
+  static const String ratingSuffix = '/ 10';
+  static const String search = 'Поиск';
+}
+
+/// Константы изображений
+class MoviePictures {
+  static CacheManager pictureCache =
+      CacheManager(Config('movieImg', stalePeriod: const Duration(days: 7)));
+}
+
+/// Константы для цветов
+class MovieColors {
+  static const Color greenColor = Color.fromRGBO(54, 164, 94, 1);
+  static const Color greyColor = Color.fromRGBO(84, 84, 84, 1);
+  static const Color cardBlackColor = Color.fromRGBO(28, 28, 28, 1.0);
+  static const Color backgroundBlackColor = Color.fromRGBO(16, 16, 16, 1.0);
+}
---
+++ lib/data/repositories/films_repository.dart
@@ -0,0 +1,53 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:project_video/app/constants.dart';
+import 'package:project_video/app/models/film_card_model.dart';
+import 'package:project_video/app/models/home_model.dart';
+import 'package:project_video/data/dtos/show_card_dto.dart';
+import 'package:project_video/data/mappers/show_mapper.dart';
+import 'package:project_video/features/dailogs/error_dialog.dart';
+
+class FilmsRepository {
+  static final Dio _dio = Dio()
+    ..interceptors.add(
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+      ),
+    );
+
+  static Future<HomeModel?> loadData(BuildContext context,
+      {required String q}) async {
+    try {
+      const String url = MovieQuery.baseUrl;
+      final Response<dynamic> response = await _dio.get<List<dynamic>>(
+        url,
+        queryParameters: <String, dynamic>{'q': q},
+      );
+
+      final dtos = <ShowCardDTO>[];
+      final responceList = response.data as List<dynamic>;
+      for (final data in responceList) {
+        dtos.add(
+          ShowCardDTO.fromJson(data as Map<String, dynamic>),
+        );
+      }
+
+      final filmsModel = <FilmCardModel>[];
+      for (final dto in dtos) {
+        filmsModel.add(dto.toDomain());
+
+        final HomeModel model = HomeModel(results: filmsModel);
+        return model;
+      }
+    } on DioError catch (error) {
+      // todo отправка в Firebase Crashlytics
+
+      // При ошибке покажем ее в диалоге
+      final statusCode = error.response?.statusCode;
+      showErrorDialog(context, error: statusCode?.toString() ?? '');
+      return null;
+    }
+  }
+}
