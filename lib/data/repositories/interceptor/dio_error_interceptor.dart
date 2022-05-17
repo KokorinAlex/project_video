d397ed23f7fa6ae3121008e52d862a1bf01938c1
KokorinAlex <a.kokorin73@mail.ru>
2022-05-17 14:21:01 +0400

    add FirebaseCrashlytics

--- lib/data/repositories/interceptor/dio_error_interceptor.dart
+++ lib/data/repositories/interceptor/dio_error_interceptor.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:project_video/app/constants.dart';
 
 class ErrorInterceptor extends Interceptor {
@@ -12,6 +13,24 @@ class ErrorInterceptor extends Interceptor {
       err.response?.statusCode?.toString() ?? MovieLocal.unknown,
       err.message,
     );
+    _sendToCrashlytics(err);
     handler.next(err);
+  }
+
+  void _sendToCrashlytics(DioError err) {
+    final customKeys = <String, String>{
+      'path': err.requestOptions.uri.path,
+      'url': err.requestOptions.uri.host,
+      'response_type': err.requestOptions.responseType.toString(),
+      'query_params': err.requestOptions.queryParameters.toString(),
+    };
+    for (final k in customKeys.keys) {
+      FirebaseCrashlytics.instance.setCustomKey(k, customKeys[k] ?? '-');
+    }
+    FirebaseCrashlytics.instance.recordError(
+      err.message,
+      err.stackTrace,
+      printDetails: true,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:project_video/app/locals/locals.dart';
@@ -14,8 +15,16 @@ import 'package:project_video/features/settings/pages/setting_page.dart';
 import 'package:flutter/material.dart';
 import 'package:project_video/locale_bloc/locale_bloc.dart';
 import 'package:project_video/locale_bloc/locale_state.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(const MyApp());
 }
 
